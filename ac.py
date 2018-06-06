9ae490df579ab1297f1f827672fc694fe4e9f31d
wxiang@gmail.com <wxiang@gmail.com>
2018-06-06 22:48:03 +0800

    Create ac.py

---
+++ ac.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+#encoding=utf-8
+
+aclist = {
+	# Change ac information here, support brand: aruba, h3c, huawei, cisco, ruijie
+	'192.168.1.1':{'username':'root', 'pwd':'password', 'enable_pwd':'enable_password', 'brand':'aruba'},
+	'192.168.1.2':{'username':'root', 'pwd':'password', 'enable_pwd':'enable_password', 'brand':'h3c'},
+	'192.168.1.3':{'username':'root', 'pwd':'password', 'enable_pwd':'enable_password', 'brand':'huawei'},
+	'192.168.1.4':{'username':'root', 'pwd':'password', 'enable_pwd':'enable_password', 'brand':'cisco'},
+	'192.168.1.5':{'username':'root', 'pwd':'password', 'enable_pwd':'enable_password', 'brand':'ruijie'},
+	}
